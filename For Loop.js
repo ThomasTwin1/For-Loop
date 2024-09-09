907245e22c4b6c8763639e96eea8403ab386ca96
ThomasTwin1 <sherrard.a.thomas4@gmail.com>
2024-09-09 05:43:17 -0400

    Add files via upload

---
+++ For Loop.js
@@ -0,0 +1,10 @@
+/*step 1. 1st and 2nd statements are checked first var i = 0; i<10; 
+  step 2. if statements are true the code inside curly braces is ran
+  step 3. The third statement ++i is ran
+  step 4. This loop continues to happen until the 2nd statement i<10; is no longer true
+  */
+
+for (var i = 0; i<10; ++i) {
+    console.log(i);
+}
+
